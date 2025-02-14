dc526e97131810a993348b1cd8a29f5fccee9be4
Nguyễn Khang <nkhang.dhkl16a1hn@sv.uneti.edu.vn>
2025-02-14 20:53:19 +0700

    Đã cập nhật câu trả lời

--- chuong_1/14_NguyenKhang.py
+++ chuong_1/14_NguyenKhang.py
@@ -14,6 +14,7 @@ def create_table():
                 PhongBan Varchar(50))''')
     print('Đã tạo bảng NhanVien')
 create_table()
+print(' ')
 # Bai 2 
 cursor.execute('''INSERT INTO NhanVien VALUES
                 (1, 'Nguyen Van A', 30, 'Ke Toan'),
@@ -31,24 +32,28 @@ cursor.execute('''INSERT INTO NhanVien VALUES
                 (13, 'Tran Thi M', 26, 'Marketing'),
                 (14, 'Pham Van N', 30, 'Nhan Su'),
                 (15, 'Hoang Thi O', 27, 'IT')''')
-
+print('Đã thêm nhân viên vào bảng  \n')
 # Bai 3 
 cursor.execute('''Select * from NhanVien''')
 print('--- Danh sách nhân viên ---')
 for row in cursor.fetchall():
     print(row)
+print('\n')
 # Bai 4
 cursor.execute('''select HoTen,Tuoi
                  from NhanVien''')
 print('--- Họ tên và tuổi nhân viên ---')
 for row in cursor.fetchall():
     print(row)
+print('\n')
+
 # Bai 5 
 cursor.execute('''select * from NhanVien 
                   where  Tuoi > 25''')
 print('Các  nhân viên có tuổi  > 25')
 for row in cursor.fetchall():
     print(row)
+print('\n')
 
 # Bai 6 
 cursor.execute('''select HoTen,max(Tuoi)
@@ -57,6 +62,8 @@ cursor.execute('''select HoTen,max(Tuoi)
 print("--- Các nhân viên có độ tuổi lớn nhất trong các phòng ban ---")
 for row in cursor.fetchall():
     print(row)
+print('\n')
+
 # Bai 7
 cursor.execute('''UPDATE NhanVien SET PhongBan = 'Marketing'
                   where HoTen = 'Le Van C'
@@ -65,6 +72,11 @@ print('-- Bảng sau khi sửa --')
 cursor.execute('select * from NhanVien')
 for row in cursor.fetchall():
     print(row)
+print('\n')
+'''Không có vấn đề gì khi thực hiện lệnh chuyển đổi thông tin nếu có thì
+ có thể là 2 người có cùng họ và tên là Le Van C và cùng làm việc trong bộ phận IT 
+ để xuất cách sửa lỗi với câu lệnh sau : UPDATE NhanVien Set PhongBan = 'IT'
+                                        where HoTen = 'Le Van C' and MaNV = ... '''
 
 # Bai 8
 cursor.execute('''delete from NhanVien 
@@ -73,6 +85,11 @@ print('-- Bảng sau khi sửa --')
 cursor.execute('select * from NhanVien')
 for row in cursor.fetchall():
     print(row)
-
+print('\n')
+cursor.execute('''select PhongBan, Count(PhongBan) as so_nv
+               from NhanVien
+               group by PhongBan''')
+print('Số nhân viên của mỗi phòng ban')
+print(cursor.fetchall())
 conn.commit()
 conn.close()
